5518d59f156d158e6356db509c893d0b31a4ff5b
amansilawat <aman.khan.silawat.aks@gmail.com>
2021-08-17 12:27:19 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # birthday-reminder--react
+
+#### Live Project
+[Live Demo](birthday-react-012.netlify.app)
---
+++ build/precache-manifest.df06b00652feb0738514b65269d9b0a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ce7456aeb61bb9e7db0032573ac34b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "346e4763f468d41de1a0",
+    "url": "/static/css/main.10d6a708.chunk.css"
+  },
+  {
+    "revision": "6e2d7a9fbc772ec3686f",
+    "url": "/static/js/2.1f771bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1f771bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "346e4763f468d41de1a0",
+    "url": "/static/js/main.d9ceedda.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
